7d7268f875be1e5316dc004f2e37e9b2157ea792
Andrea2875 <andrea.tan@binus.ac.id>
2025-09-02 11:36:26 +0700

    assignment.c

--- assignment.c
+++ assignment.c
@@ -1,2 +1,6 @@
-printf("My name is Andrea\n")
-printf("My favourite programming language is C language\n")
+#include<stdio.h>
+int main(){
+printf("My name is Andrea\n");
+printf("My favourite programming language is C language\n");
+return 0;
+}
